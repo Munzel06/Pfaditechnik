5b96aad7133066150e73ce48a77c5325324d9aae
Munzel <bardillandrea@gmail.com>
2024-06-04 18:55:22 +0200

    I have done this on purpose

--- app/src/main/java/com/example/pfaditechnik/MainActivity.java
+++ app/src/main/java/com/example/pfaditechnik/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Mal schauen ob das funktioniert
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
