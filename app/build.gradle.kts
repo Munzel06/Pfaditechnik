71aa60a65a5fb0343e27a17c77b9a072c9074289
Munzel06 <bardillandrea@gmail.com>
2024-12-12 08:20:36 +0100

    Bereit machen für Update auf Google Play Store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.pfaditechnik"
+        applicationId = "com.pfadi.pfaditechnik"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
